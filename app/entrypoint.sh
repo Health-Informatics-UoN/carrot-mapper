f781206708d7a152e22de6d0aefe6252cef49b28
andrewthien <anretrithien@gmail.com>
2025-01-13 15:24:39 +0000

    refine Dockerfile and Docker compose

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Run npm build
-export PATH=$PATH:/home/django/.nvm/versions/node/v12.18.3/bin
-
 # Wait until DB is available
 wait-for-it ${COCONNECT_DB_HOST}:${COCONNECT_DB_PORT} -- echo "Database is ready! Listening on ${COCONNECT_DB_HOST}:${COCONNECT_DB_PORT}"
 
