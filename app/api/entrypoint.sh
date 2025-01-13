4535b388d841ebdf3ef1282a21cd1dfa9f0904c1
andrewthien <anretrithien@gmail.com>
2025-01-13 17:02:32 +0000

    move the entrypoint

--- app/api/entrypoint.sh
+++ app/api/entrypoint.sh
@@ -3,13 +3,12 @@
 # Wait until DB is available
 wait-for-it ${COCONNECT_DB_HOST}:${COCONNECT_DB_PORT} -- echo "Database is ready! Listening on ${COCONNECT_DB_HOST}:${COCONNECT_DB_PORT}"
 
-cd /api
 
 # Collect static files for serving
 rm -rf staticfiles
 mkdir staticfiles
-python /api/manage.py collectstatic
-python /api/manage.py migrate
+python manage.py collectstatic
+python manage.py migrate
 
 # Set tmp dir to be in-memory for speed. Pass logs to stdout/err as Docker will expect them there
 gunicorn --config gunicorn.conf.py --worker-tmp-dir /dev/shm --timeout 600 --log-file=- --bind :8000 --workers 3 config.wsgi:application --reload
