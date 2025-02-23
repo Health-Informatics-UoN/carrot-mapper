fa8e33be299c5e189182fab95e588f39d804290b
Karthikeyan Sivakumar <126179797+Karthi-DStech@users.noreply.github.com>
2025-02-23 02:56:18 +0000

    Automatic-Seeding-Feature (#947)

--- app/api/entrypoint.sh
+++ app/api/entrypoint.sh
@@ -9,6 +9,7 @@ rm -rf staticfiles
 mkdir staticfiles
 python manage.py collectstatic
 python manage.py migrate
+python manage.py automatic_seeding_data
 
 # Set tmp dir to be in-memory for speed. Pass logs to stdout/err as Docker will expect them there
 gunicorn --config gunicorn.conf.py --worker-tmp-dir /dev/shm --timeout 600 --log-file=- --bind :8000 --workers 3 config.wsgi:application --reload
---
+++ app/shared/shared/mapping/management/commands/automatic_seeding_data.py
@@ -0,0 +1,39 @@
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+from shared.mapping.models import OmopTable
+from shared.files.models import FileType
+
+
+class Command(BaseCommand):
+    help = "Set automatic seeding of data if it is not already seeded."
+
+    class TableMappingSeedingError(Exception):
+        pass
+
+    class FileTypeSeedingError(Exception):
+        pass
+
+    def seed_omop_tables(self):
+        try:
+            if not OmopTable.objects.exists():
+                self.stdout.write("Seeding Omop Tables...")
+                call_command("loaddata", "mapping")
+            else:
+                self.stdout.write("Omop Tables already seeded.")
+        except Exception as e:
+            raise self.TableMappingSeedingError(f"Error seeding Omop Tables: {e}")
+
+    def seed_file_types(self):
+        try:
+            if not FileType.objects.exists():
+                self.stdout.write("Seeding File Types...")
+                call_command("loaddata", "filetypes")
+            else:
+                self.stdout.write("File Types data is already seeded.")
+        except Exception as e:
+            raise self.FileTypeSeedingError(f"Error seeding File Types: {e}")
+
+    def handle(self, *args, **options):
+        self.seed_omop_tables()
+        self.seed_file_types()
+        self.stdout.write(self.style.SUCCESS("Automatic seeding complete."))
