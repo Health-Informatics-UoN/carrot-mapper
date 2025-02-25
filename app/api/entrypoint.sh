c657462264afa7f0926cc21d7eb27aa292f34e5f
Peter LaValle <peter.lavalle@gmail.com>
2025-02-25 17:39:34 +0000

    that should create the users

--- app/api/entrypoint.sh
+++ app/api/entrypoint.sh
@@ -9,6 +9,7 @@ rm -rf staticfiles
 mkdir staticfiles
 python manage.py collectstatic
 python manage.py migrate
+python initial_user.py
 
 # Set tmp dir to be in-memory for speed. Pass logs to stdout/err as Docker will expect them there
 gunicorn --config gunicorn.conf.py --worker-tmp-dir /dev/shm --timeout 600 --log-file=- --bind :8000 --workers 3 config.wsgi:application --reload
