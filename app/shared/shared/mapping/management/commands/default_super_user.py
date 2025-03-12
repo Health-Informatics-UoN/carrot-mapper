0cce504ae6ee9f2a5d6932d931e4156574c46456
Peter LaValle <peter.lavalle@gmail.com>
2025-03-12 18:36:43 +0000

    improve logging messages
    adjusted the log messages for "add user" and "don't add user"

--- app/shared/shared/mapping/management/commands/default_super_user.py
+++ app/shared/shared/mapping/management/commands/default_super_user.py
@@ -7,9 +7,9 @@ import os
 
 
 class Command(BaseCommand):
-    """Create a default superuser if no users exist"""
+    """Create a default superuser if and only if no users exist"""
 
-    help = "Create a default superuser if no users exist"
+    help = "Create a default superuser if and only if no users exist"
 
     from django.contrib.auth import get_user_model
 
@@ -23,22 +23,24 @@ class Command(BaseCommand):
         """primary functionality"""
 
         user_count = self.users.objects.count()
-        if 0 != user_count:
-            if 1 == user_count:
-                user_count = (
-                    "There is already a user - default superuser will not be added"
-                )
-            else:
-                user_count = f"There are {user_count} users  - default superuser will not be added"
-            self.stdout.write(self.style.SUCCESS(user_count))
-        else:
-            self.stdout.write(
-                self.style.SUCCESS(
-                    f"No users in the database - {self.settings.SUPERUSER_DEFAULT_NAME} / {self.settings.SUPERUSER_DEFAULT_EMAIL} will be created as a super user."
-                )
+        if user_count > 0:
+            message = (
+                "There is already a user - default superuser will not be added"
+                if user_count == 1
+                else f"There are {user_count} users - default superuser will not be added"
             )
+            self.stdout.write(self.style.SUCCESS(message))
+            return
+        else:
+            # execute the creation command
             self.users.objects.create_superuser(
                 self.settings.SUPERUSER_DEFAULT_NAME,
                 self.settings.SUPERUSER_DEFAULT_EMAIL,
                 self.settings.SUPERUSER_DEFAULT_PASSWORD,
+            )
+            # log the user's details
+            self.stdout.write(
+                self.style.SUCCESS(
+                    f"Superuser successfully created with Username='{self.settings.SUPERUSER_DEFAULT_NAME}', Password='{self.settings.SUPERUSER_DEFAULT_PASSWORD}'"
+                )
             )
