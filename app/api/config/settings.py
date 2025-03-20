634f21897ace615a42930578502d2bc60a4bc7ac
Karthikeyan Sivakumar <126179797+Karthi-DStech@users.noreply.github.com>
2025-03-20 17:14:35 +0000

    format settings.py

--- app/api/config/settings.py
+++ app/api/config/settings.py
@@ -248,7 +248,6 @@ SUPERUSER_DEFAULT_USERNAME = os.getenv("SUPERUSER_DEFAULT_USERNAME", None)
 # Storage Type Variable
 STORAGE_TYPE = os.getenv("STORAGE_TYPE", "azure")
 
-
 # Azure Storage Connection String
 azure_storage_conn_string = "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://azurite:10000/devstoreaccount1;QueueEndpoint=http://azurite:10001/devstoreaccount1;TableEndpoint=http://azurite:10002/devstoreaccount1;"
 
