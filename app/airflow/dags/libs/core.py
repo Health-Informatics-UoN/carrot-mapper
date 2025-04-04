0e8991df28485d6d671d7ec984107d238ffee4e3
AndrewThien <anretrithien@gmail.com>
2025-04-04 10:26:51 +0100

    refactor and add dest date fields ids

--- app/airflow/dags/libs/core.py
+++ app/airflow/dags/libs/core.py
@@ -20,17 +20,14 @@ def find_and_create_standard_concepts(**kwargs):
                 "Invalid field_vocab_pair: requires sr_field_id and vocabulary_id"
             )
 
-        find_std_concept_query = f"""
+        # Step 1: Form base table with columns 1 & 2 (sr_value_id and standard_concept_id)
+        step1_query = f"""
         DROP TABLE IF EXISTS temp_standard_concepts;
         CREATE TABLE temp_standard_concepts AS
         SELECT
             srv.id AS sr_value_id,
-            c2.concept_id AS standard_concept_id,
-            ot.id AS dest_table_id,
-            opf.id AS dest_person_field_id
-        -- Column 1: sr_value_id from scanreportvalue table
+            c2.concept_id AS standard_concept_id
         FROM mapping_scanreportvalue srv
-        -- Column 2: concept_id from vocabulary table which fullfiled conditions
         JOIN omop.concept c1 ON
             c1.concept_code = srv.value AND
             c1.vocabulary_id = '{vocabulary_id}'
@@ -40,7 +37,20 @@ def find_and_create_standard_concepts(**kwargs):
         JOIN omop.concept c2 ON
             c2.concept_id = cr.concept_id_2 AND
             c2.standard_concept = 'S'
-        -- Column 3: destination_table_id from omoptable table
+        WHERE srv.scan_report_field_id = {sr_field_id};
+        """
+        pg_hook.run(step1_query)
+        
+        # Step 2: Add columns 3 & 4 (dest_table_id and dest_person_field_id)
+        step2_query = """
+        ALTER TABLE temp_standard_concepts 
+        ADD COLUMN dest_table_id INTEGER,
+        ADD COLUMN dest_person_field_id INTEGER;
+        
+        -- Update destination table ID
+        UPDATE temp_standard_concepts tsc
+        SET dest_table_id = ot.id
+        FROM omop.concept c2
         LEFT JOIN mapping_omoptable ot ON
             CASE c2.domain_id
                 WHEN 'Observation' THEN 'observation'
@@ -53,15 +63,46 @@ def find_and_create_standard_concepts(**kwargs):
                 -- TODO: plan for other domains: Death, Specimen, etc. and for the case when the domain is not supported
                 ELSE LOWER(c2.domain_id)
             END = ot.table
-        -- Column 4: Dest. OMOP field for Person ID
-        LEFT JOIN mapping_omopfield opf ON
-            opf.table_id = ot.id AND
-            opf.field = 'person_id'
-        -- Supporting the first column
-        WHERE srv.scan_report_field_id = {sr_field_id};
+        WHERE c2.concept_id = tsc.standard_concept_id;
+        
+        -- Update person field ID
+        UPDATE temp_standard_concepts tsc
+        SET dest_person_field_id = opf.id
+        FROM mapping_omopfield opf
+        WHERE opf.table_id = tsc.dest_table_id
+        AND opf.field = 'person_id';
         """
-
-        pg_hook.run(find_std_concept_query)
+        pg_hook.run(step2_query)
+        
+        # Step 3: Add columns 5, 6, 7 (date fields) with simpler implementation
+        step3_query = """
+        ALTER TABLE temp_standard_concepts 
+        ADD COLUMN dest_date_field_id INTEGER,
+        ADD COLUMN dest_start_date_field_id INTEGER,
+        ADD COLUMN dest_end_date_field_id INTEGER;
+        
+        -- Add all date fields in one go
+        WITH date_field_analysis AS (
+            SELECT 
+                tsc.sr_value_id,
+                MIN(df.id) AS min_id,
+                MAX(df.id) AS max_id,
+                COUNT(*) AS count
+            FROM temp_standard_concepts tsc
+            JOIN mapping_omopfield df ON
+                df.table_id = tsc.dest_table_id AND
+                df.field LIKE '%datetime'
+            GROUP BY tsc.sr_value_id
+        )
+        UPDATE temp_standard_concepts tsc
+        SET 
+            dest_date_field_id = CASE WHEN dfa.count = 1 THEN dfa.min_id ELSE NULL END,
+            dest_start_date_field_id = CASE WHEN dfa.count > 1 THEN dfa.min_id ELSE NULL END,
+            dest_end_date_field_id = CASE WHEN dfa.count > 1 THEN dfa.max_id ELSE NULL END
+        FROM date_field_analysis dfa
+        WHERE dfa.sr_value_id = tsc.sr_value_id;
+        """
+        pg_hook.run(step3_query)
 
         create_concept_query = """
         -- Insert standard concepts for field values (only if they don't already exist)
