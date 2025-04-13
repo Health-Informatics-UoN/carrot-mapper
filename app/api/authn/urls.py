b0c0bd38af29bd8b336515b9181f6f0558178560
Brian <kiirubrian21@gmail.com>
2025-04-13 19:08:18 +0100

    Fix black formatting

--- app/api/authn/urls.py
+++ app/api/authn/urls.py
@@ -1,21 +1,24 @@
 from dj_rest_auth.jwt_auth import get_refresh_view
 from dj_rest_auth.registration.views import RegisterView
-from dj_rest_auth.views import (LoginView, 
+from dj_rest_auth.views import (
+    LoginView,
     LogoutView,
     UserDetailsView,
     PasswordResetConfirmView,
 )
 from django.urls import path
 from rest_framework_simplejwt.views import TokenVerifyView
-from .views import DirectPasswordResetView, CSRFTokenView
 
+from .views import DirectPasswordResetView, CSRFTokenView
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="rest_register"),
     path("login/", LoginView.as_view(), name="rest_login"),
     path("logout/", LogoutView.as_view(), name="rest_logout"),
     path(
-        "password/reset/", DirectPasswordResetView.as_view(), name="rest_password_reset"
+        "password/reset/",
+        DirectPasswordResetView.as_view(),
+        name="rest_password_reset",
     ),
     path(
         "auth/password-reset-confirm/<uidb64>/<token>/",
