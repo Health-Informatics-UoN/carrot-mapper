870a50e50313308af73b17f86b3d1665f35d199e
Brian <kiirubrian21@gmail.com>
2025-04-13 19:05:35 +0100

    Fix black formatting

--- app/api/authn/urls.py
+++ app/api/authn/urls.py
@@ -1,7 +1,6 @@
 from dj_rest_auth.jwt_auth import get_refresh_view
 from dj_rest_auth.registration.views import RegisterView
-from dj_rest_auth.views import (
-    LoginView, 
+from dj_rest_auth.views import (LoginView, 
     LogoutView,
     UserDetailsView,
     PasswordResetConfirmView,
