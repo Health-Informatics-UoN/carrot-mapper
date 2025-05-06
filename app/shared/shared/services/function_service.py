f8f649e9927b4b55f8a2dd767467b301fbc156bf
AndrewThien <anretrithien@gmail.com>
2025-05-06 11:53:25 +0100

    reform the bridge

--- app/shared/shared/services/function_service.py
+++ app/shared/shared/services/function_service.py
@@ -11,6 +11,7 @@ from shared.services.field_vocab_mappings import get_field_vocab_mappings
 from shared.services.rules import (
     delete_mapping_rules,
 )
+from django.conf import settings
 
 
 class FUNCTION_TYPE(StrEnum):
@@ -26,14 +27,16 @@ class FunctionService:
         """
         Service for triggering functions.
         """
-        self._function_type = os.getenv("FUNCTION_TYPE")
+        self._function_type = settings.FUNCTION_TYPE
 
     def trigger_auto_mapping(
         self,
         scan_report: ScanReport,
         table: ScanReportTable,
         data_dictionary_name: Optional[str] = None,
+        # Placeholder for the issue #623 or enabling users to disable/enable reuse concepts function
         trigger_reuse_concepts: bool = True,
+        # TODO: add death_table config here
     ):
         """
         Trigger the automapping process.
@@ -47,8 +50,8 @@ class FunctionService:
                 "table_id": table.pk,
                 "data_dictionary_blob": data_dictionary_name,
             }
-            base_url = f"{os.getenv('WORKERS_URL')}"
-            trigger = f"/api/orchestrators/{os.getenv('WORKERS_RULES_NAME')}?code={os.getenv('WORKERS_RULES_KEY')}"
+            base_url = f"{settings.WORKERS_URL}"
+            trigger = f"/api/orchestrators/{settings.WORKERS_RULES_NAME}?code={settings.WORKERS_RULES_KEY}"
             try:
                 # Then send the request to workers, in case there is error, the Job record was created already
                 response = requests.post(urljoin(base_url, trigger), json=msg)
@@ -58,86 +61,37 @@ class FunctionService:
                 logging.error(f"HTTP Trigger failed: {e}")
 
         elif self._function_type == FUNCTION_TYPE.AIRFLOW:
-            # TODO: add update Generate rules to avoid UI breaking
-            # TODO: make sub functions for each step
-            # TODO: make auth configurable for airflow
-            # TODO: what if I don't have DD but want to update the person ID and date event?
-            # TODO: refresh the mapping rules for manual mapping or for the table without field-vocab pairs or for a SR doesn't have reuse demand
-            base_url = f"{os.getenv('AIRFLOW_URL')}"
-            # Send to functions
-            if data_dictionary_name or get_field_vocab_mappings(
-                data_dictionary_name, table
-            ):
-                vocab_concepts_msg = {
-                    "conf": {
-                        "scan_report_id": scan_report.pk,
-                        "table_id": table.pk,
-                        "person_id_field": table.person_id.pk,
-                        "date_event_field": table.date_event.pk,
-                        "field_vocab_pairs": get_field_vocab_mappings(
-                            data_dictionary_name, table
-                        ),
-                    }
+            base_url = f"{settings.AIRFLOW_URL}"
+            # Send to Airflow workflow
+            auto_mapping_trigger = (
+                f"/api/v1/dags/{settings.AIRFLOW_AUTO_MAPPING_DAG_ID}/dagRuns"
+            )
+            body = {
+                "conf": {
+                    "parent_dataset_id": scan_report.parent_dataset.pk,
+                    "scan_report_id": scan_report.pk,
+                    "table_id": table.pk,
+                    "person_id_field": table.person_id.pk,
+                    "date_event_field": table.date_event.pk,
+                    "field_vocab_pairs": get_field_vocab_mappings(
+                        data_dictionary_name, table
+                    ),
+                    "trigger_reuse_concepts": trigger_reuse_concepts,
                 }
-                vocab_concepts_trigger = (
-                    f"/api/v1/dags/{os.getenv('AIRFLOW_VOCAB_CONCEPTS_DAG_ID')}/dagRuns"
+            }
+            try:
+                # Then send the request to workers, in case there is error, the Job record was created already
+                response = requests.post(
+                    urljoin(base_url, auto_mapping_trigger),
+                    json=body,
+                    auth=HTTPBasicAuth(
+                        settings.AIRFLOW_ADMIN_USERNAME, settings.AIRFLOW_ADMIN_PASSWORD
+                    ),
                 )
-                try:
-                    # Then send the request to workers, in case there is error, the Job record was created already
-                    response = requests.post(
-                        urljoin(base_url, vocab_concepts_trigger),
-                        json=vocab_concepts_msg,
-                        auth=HTTPBasicAuth(
-                            os.getenv("AIRFLOW_USERNAME"), os.getenv("AIRFLOW_PASSWORD")
-                        ),
-                    )
-                    response.raise_for_status()
-                except requests.exceptions.HTTPError as e:
-                    logging.error(f"HTTP Trigger failed: {e}")
-            else:
-                Job.objects.create(
-                    scan_report=scan_report,
-                    scan_report_table=table,
-                    stage=JobStage.objects.get(value="BUILD_CONCEPTS_FROM_DICT"),
-                    status=StageStatus.objects.get(value="COMPLETE"),
-                    details=f"Skipped, because no data dictionary provided or no vocab ID is set for the table {table.name}.",
-                )
+                response.raise_for_status()
+            except requests.exceptions.HTTPError as e:
+                logging.error(f"HTTP Trigger failed: {e}")
 
-            if trigger_reuse_concepts:
-                reuse_concepts_msg = {
-                    "conf": {
-                        "parent_dataset_id": scan_report.parent_dataset.pk,
-                        "scan_report_id": scan_report.pk,
-                        "table_id": table.pk,
-                        "person_id_field": table.person_id.pk,
-                        "date_event_field": table.date_event.pk,
-                        "has_data_dictionary": True if data_dictionary_name else False,
-                    }
-                }
-                reuse_concepts_trigger = (
-                    f"/api/v1/dags/{os.getenv('AIRFLOW_REUSE_CONCEPTS_DAG_ID')}/dagRuns"
-                )
-                try:
-                    # Then send the request to workers, in case there is error, the Job record was created already
-                    response = requests.post(
-                        urljoin(base_url, reuse_concepts_trigger),
-                        json=reuse_concepts_msg,
-                        auth=HTTPBasicAuth(
-                            os.getenv("AIRFLOW_USERNAME"), os.getenv("AIRFLOW_PASSWORD")
-                        ),
-                    )
-                    response.raise_for_status()
-
-                except requests.exceptions.HTTPError as e:
-                    logging.error(f"HTTP Trigger failed: {e}")
-            else:
-                Job.objects.create(
-                    scan_report=scan_report,
-                    scan_report_table=table,
-                    stage=JobStage.objects.get(value="REUSE_CONCEPTS"),
-                    status=StageStatus.objects.get(value="COMPLETE"),
-                    details=f"Skipped as specified in the request.",
-                )
         else:
             raise ValueError(
                 "Function type not supported. Only Airflow or Azure Function is supported."
