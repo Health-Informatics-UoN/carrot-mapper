1e93985a06268302200cc363e9b094e12c9c202d
Brian <kiirubrian21@gmail.com>
2025-05-06 12:50:17 +0100

    Move Swagger Urls to App Urls

--- app/api/config/urls.py
+++ app/api/config/urls.py
@@ -1,24 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
-from drf_spectacular.views import (
-    SpectacularAPIView,
-    SpectacularSwaggerView,
-    SpectacularRedocView,
-)
 
 urlpatterns = [
     path("api/", include("api.urls")),
     path("api/auth/", include("authn.urls")),
     path("admin/", admin.site.urls),
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-    path(
-        "api/schema/swagger-ui/",
-        SpectacularSwaggerView.as_view(url_name="schema"),
-        name="schema-swagger-ui",
-    ),
-    path(
-        "api/schema/redoc/",
-        SpectacularRedocView.as_view(url_name="schema"),
-        name="schema-redoc",
-    ),
 ]
