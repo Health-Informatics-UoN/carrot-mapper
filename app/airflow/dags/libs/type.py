1bbc6ac2322f07dc42c459e20b42a1f772438221
AndrewThien <anretrithien@gmail.com>
2025-05-20 17:22:15 +0100

    add type

---
+++ app/airflow/dags/libs/type.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass
+from typing import Any, Callable
+
+
+@dataclass
+class FileHandlerConfig:
+    handler: Callable[[Any], Any]
+    file_type_value: str
+    file_extension: str
