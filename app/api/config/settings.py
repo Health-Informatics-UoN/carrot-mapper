61269bae37a7f6fa26bceaedbd93a8009a086f7a
AndrewThien <anretrithien@gmail.com>
2025-05-21 13:16:00 +0100

    test

--- app/api/config/settings.py
+++ app/api/config/settings.py
@@ -210,7 +210,7 @@ SPECTACULAR_SETTINGS = {
 }
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_URL = "/static/"
+STATIC_URL = "/static"
 
 LOGIN_REDIRECT_URL = "/scanreports/"
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
