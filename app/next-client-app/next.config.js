9faf469b72a5cb78463ee90fcd918ba0e46eab9c
Brian <kiirubrian21@gmail.com>
2025-05-29 16:01:13 +0100

    Made bodySizeLimit configurable

--- app/next-client-app/next.config.js
+++ app/next-client-app/next.config.js
@@ -4,12 +4,17 @@
 // Allows for multiple allowedOrigins in one environment
 const allowedOrigins = process.env.BACKEND_ORIGIN?.split(",");
 
+// Allows configuration of body size limit via env (in bytes)
+const bodySizeLimit = process.env.NEXT_SERVER_ACTION_BODY_SIZE_LIMIT
+  ? parseInt(process.env.NEXT_SERVER_ACTION_BODY_SIZE_LIMIT, 10)
+  : 20971520; // Default to 20MB if not set
+
 const nextConfig = {
   output: "standalone",
   experimental: {
     serverActions: {
-      allowedOrigins: allowedOrigins,
-      bodySizeLimit: 10485760, // 10MB limit for server actions
+      allowedOrigins,
+      bodySizeLimit,
     },
   },
   async redirects() {
