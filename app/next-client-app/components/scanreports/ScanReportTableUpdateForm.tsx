0969deebbbbf0a84fe98511a1d9cb9b4a729fd84
AndrewThien <anretrithien@gmail.com>
2025-06-04 12:09:29 +0100

    use checkbox and move the question up

--- app/next-client-app/components/scanreports/ScanReportTableUpdateForm.tsx
+++ app/next-client-app/components/scanreports/ScanReportTableUpdateForm.tsx
@@ -7,10 +7,10 @@ import { toast } from "sonner";
 import { FormDataFilter } from "../form-components/FormikUtils";
 import { Form, Formik } from "formik";
 import { FormikSelect } from "../form-components/FormikSelect";
-import { Switch } from "../ui/switch";
 import { Label } from "../ui/label";
 import { Tooltips } from "../core/Tooltips";
 import { useRouter } from "next/navigation";
+import { Checkbox } from "../ui/checkbox";
 
 interface FormData {
   personId: number | null;
@@ -110,7 +110,30 @@ export function ScanReportTableUpdateForm({
                 isDisabled={!canUpdate}
               />
             </div>
-
+            {process.env.NEXT_PUBLIC_ENABLE_REUSE_TRIGGER_OPTION == "true" && (
+              <div className="flex gap-2 mt-2 items-center">
+                <h3 className="flex">
+                  Do you want to trigger the reuse of existing concepts?
+                  <Tooltips
+                    content="If YES, concepts added to other scan reports which are in same
+                      parent dataset will be reused, based
+                      on the matching value and field. This feature may make the
+                      auto mapping process longer to run."
+                  />
+                </h3>
+                <Checkbox
+                  checked={values.triggerReuse}
+                  onCheckedChange={(checked) => {
+                    setFieldValue("triggerReuse", checked);
+                  }}
+                  disabled={!canUpdate}
+                  className="size-5"
+                />
+                <Label className="text-lg">
+                  {values.triggerReuse === true ? "YES" : "NO"}
+                </Label>
+              </div>
+            )}
             <div className="flex mt-3">
               <Button
                 type="submit"
@@ -124,29 +147,6 @@ export function ScanReportTableUpdateForm({
                     to update the details of this scan report table."
               />
             </div>
-            {process.env.NEXT_PUBLIC_ENABLE_REUSE_TRIGGER_OPTION == "true" && (
-              <div className="flex gap-2 mt-2">
-                <h3 className="flex">
-                  Do you want to trigger the reuse of existing concepts?
-                  <Tooltips
-                    content="If YES, concepts added to other scan reports which are in same
-                      parent dataset will be reused, based
-                      on the matching value and field. This feature may make the
-                      auto mapping process longer to run."
-                  />
-                </h3>
-                <Switch
-                  checked={values.triggerReuse}
-                  onCheckedChange={(checked) => {
-                    setFieldValue("triggerReuse", checked);
-                  }}
-                  disabled={!canUpdate}
-                />
-                <Label className="text-lg">
-                  {values.triggerReuse === true ? "YES" : "NO"}
-                </Label>
-              </div>
-            )}
           </div>
         </Form>
       )}
