53f917f9c55b5cd0b7d18ce2c8d194ee5887c4b9
Brian <kiirubrian21@gmail.com>
2025-06-04 12:02:22 +0100

    Add background color on hover

--- app/next-client-app/components/data-table/index.tsx
+++ app/next-client-app/components/data-table/index.tsx
@@ -160,6 +160,7 @@ export function DataTable<TData, TValue>({
                 <TableRow
                   key={row.id}
                   data-state={row.getIsSelected() && "selected"}
+                  className="hover:bg-muted transition-colors"
                 >
                   {row.getVisibleCells().map((cell) => (
                     <TableCell key={cell.id}>
