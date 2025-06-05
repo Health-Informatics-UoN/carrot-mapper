26a6c5131db52824bb328b62929f79f9b204cefa
AndrewThien <anretrithien@gmail.com>
2025-06-05 10:03:05 +0100

    test env var at next.config

--- app/next-client-app/next.config.js
+++ app/next-client-app/next.config.js
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
-const { makeEnvPublic } = require("next-runtime-env");
-makeEnvPublic("NEXT_SERVER_ACTION_BODY_SIZE_LIMIT");
 
 // Local BACKEND_ORIGIN="127.0.0.1:8000"
 // Allows for multiple allowedOrigins in one environment
 const allowedOrigins = process.env.BACKEND_ORIGIN?.split(",");
 
 // Allows configuration of body size limit via env (in bytes)
-const bodySizeLimit = process.env.NEXT_SERVER_ACTION_BODY_SIZE_LIMIT
-  ? parseInt(process.env.NEXT_SERVER_ACTION_BODY_SIZE_LIMIT, 10)
+const bodySizeLimit = process.env.BODY_SIZE_LIMIT
+  ? parseInt(process.env.BODY_SIZE_LIMIT, 10)
   : 15728640; // Default to 15MB if not set
 
 const nextConfig = {
