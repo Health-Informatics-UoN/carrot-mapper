d53bdae74d1428bc9e08f2a24990cf4ab01001c0
AndrewThien <anretrithien@gmail.com>
2025-06-19 12:35:20 +0100

    formatting

--- app/shared/shared/mapping/migrations/0008_remove_scanreportfield_fraction_empty_and_more.py
+++ app/shared/shared/mapping/migrations/0008_remove_scanreportfield_fraction_empty_and_more.py
@@ -6,32 +6,32 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('mapping', '0007_alter_dataset_hidden_alter_mappingstatus_value_and_more'),
+        ("mapping", "0007_alter_dataset_hidden_alter_mappingstatus_value_and_more"),
     ]
 
     operations = [
         migrations.RemoveField(
-            model_name='scanreportfield',
-            name='fraction_empty',
+            model_name="scanreportfield",
+            name="fraction_empty",
         ),
         migrations.RemoveField(
-            model_name='scanreportfield',
-            name='fraction_unique',
+            model_name="scanreportfield",
+            name="fraction_unique",
         ),
         migrations.RemoveField(
-            model_name='scanreportfield',
-            name='max_length',
+            model_name="scanreportfield",
+            name="max_length",
         ),
         migrations.RemoveField(
-            model_name='scanreportfield',
-            name='nrows',
+            model_name="scanreportfield",
+            name="nrows",
         ),
         migrations.RemoveField(
-            model_name='scanreportfield',
-            name='nrows_checked',
+            model_name="scanreportfield",
+            name="nrows_checked",
         ),
         migrations.RemoveField(
-            model_name='scanreportfield',
-            name='nunique_values',
+            model_name="scanreportfield",
+            name="nunique_values",
         ),
     ]
