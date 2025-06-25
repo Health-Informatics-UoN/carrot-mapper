12942c43c4cc076d7e3076b6a8c568f5e5dab3dc
AndrewThien <anretrithien@gmail.com>
2025-06-25 11:05:44 +0100

    make domains based on allowed domains

--- app/next-client-app/constants/domains.ts
+++ app/next-client-app/constants/domains.ts
@@ -5,16 +5,6 @@ export const domains = [
     conceptId: 19,
   },
   {
-    id: "Cost",
-    name: "Cost",
-    conceptId: 581456,
-  },
-  {
-    id: "Currency",
-    name: "Currency",
-    conceptId: 34,
-  },
-  {
     id: "Device",
     name: "Device",
     conceptId: 17,
@@ -23,11 +13,6 @@ export const domains = [
     id: "Drug",
     name: "Drug",
     conceptId: 13,
-  },
-  {
-    id: "Episode",
-    name: "Episode",
-    conceptId: 32527,
   },
   {
     id: "Ethnicity",
@@ -40,39 +25,9 @@ export const domains = [
     conceptId: 2,
   },
   {
-    id: "Geography",
-    name: "Geographical object",
-    conceptId: 32558,
-  },
-  {
-    id: "Language",
-    name: "Language",
-    conceptId: 33068,
-  },
-  {
     id: "Measurement",
     name: "Measurement",
     conceptId: 21,
-  },
-  {
-    id: "Meas Value",
-    name: "Measurement Value",
-    conceptId: 24,
-  },
-  {
-    id: "Metadata",
-    name: "Metadata",
-    conceptId: 7,
-  },
-  {
-    id: "Modifier",
-    name: "Modifier",
-    conceptId: 12,
-  },
-  {
-    id: "Note",
-    name: "Note",
-    conceptId: 5085,
   },
   {
     id: "Observation",
@@ -85,24 +40,9 @@ export const domains = [
     conceptId: 10,
   },
   {
-    id: "Provider",
-    name: "Provider Specialty",
-    conceptId: 33,
-  },
-  {
     id: "Race",
     name: "Race",
     conceptId: 3,
-  },
-  {
-    id: "Regimen",
-    name: "Treatment Regimen",
-    conceptId: 32687,
-  },
-  {
-    id: "Relationship",
-    name: "Relationship",
-    conceptId: 31,
   },
   {
     id: "Spec Anatomic Site",
@@ -110,28 +50,8 @@ export const domains = [
     conceptId: 38,
   },
   {
-    id: "Spec Disease Status",
-    name: "Specimen Disease Status",
-    conceptId: 39,
-  },
-  {
     id: "Specimen",
     name: "Specimen",
     conceptId: 36,
-  },
-  {
-    id: "Type Concept",
-    name: "Type Concept",
-    conceptId: 58,
-  },
-  {
-    id: "Unit",
-    name: "Unit",
-    conceptId: 16,
-  },
-  {
-    id: "Visit",
-    name: "Visit",
-    conceptId: 8,
   },
 ];
