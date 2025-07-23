d8001335037bf65370c7a3046022ee07e8b9666a
Brian Kiiru Kim <kiirubrian21@gmail.com>
2025-07-23 15:36:46 +0100

    Added search-based recommendations to auto_mapping DAG. (#1159)

--- app/airflow/dags/libs/settings.py
+++ app/airflow/dags/libs/settings.py
@@ -10,5 +10,8 @@ AIRFLOW_VAR_MINIO_SECRET_KEY = os.getenv("AIRFLOW_VAR_MINIO_SECRET_KEY", "")
 # DEBUG MODE: True or False
 AIRFLOW_DEBUG_MODE = os.getenv("AIRFLOW_DEBUG_MODE", "false").lower()
 
+# SEARCH ENABLED: Controls whether search recommendations DAG is enabled
+SEARCH_ENABLED = os.getenv("SEARCH_ENABLED", "false").lower()
+
 # Timedelta for dagrun_timeout in minutes
 AIRFLOW_DAGRUN_TIMEOUT = os.getenv("AIRFLOW_DAGRUN_TIMEOUT", 60)
