63616a69e8b6b78e556f69f45cd06a017fcc3ce3
Andy Rae <1127507+AndyRae@users.noreply.github.com>
2025-08-01 09:47:25 +0100

    Stop shouting

--- app/next-client-app/components/scanreports/ScanReportTableUpdateForm.tsx
+++ app/next-client-app/components/scanreports/ScanReportTableUpdateForm.tsx
@@ -126,11 +126,11 @@ export function ScanReportTableUpdateForm({
                         />
                       </FormControl>
                       <span className="text-sm">
-                        {values.triggerReuse === true ? "YES" : "NO"}
+                        {values.triggerReuse === true ? "Yes" : "No"}
                       </span>
                     </div>
                     <FormDescription>
-                      If YES, concepts added to other scan reports which are in same parent dataset will be reused, based on the matching value and field. This feature may make the auto mapping process longer to run.
+                      If Yes, concepts added to other scan reports which are in same parent dataset will be reused, based on the matching value and field. This feature may make the auto mapping process longer to run.
                     </FormDescription>
                     <FormMessage />
                   </FormItem>
