07acf62cea7420ca915e16cfef7b489a2f3baaa1
AndrewThien <anretrithien@gmail.com>
2025-08-11 14:40:51 +0100

    hot fix for text wrap

--- app/next-client-app/components/concepts/concept-tags.tsx
+++ app/next-client-app/components/concepts/concept-tags.tsx
@@ -12,7 +12,7 @@ const LazyBadge = lazy(() =>
 // Using react.memo and react.lazy to prevent loading unnecessary tags
 export const ConceptTags = React.memo(function ConceptTags({
   concepts,
-  deleteSR
+  deleteSR,
 }: {
   concepts: Concept[];
   deleteSR: any;
@@ -32,7 +32,7 @@ export const ConceptTags = React.memo(function ConceptTags({
   };
 
   return concepts && concepts.length > 0 ? (
-    <div className="flex flex-col items-start w-[250px]">
+    <div className="flex flex-col items-start max-w-[500px] whitespace-pre-line text-pretty">
       {concepts.map((concept) => (
         <a
           key={concept.concept_code}
