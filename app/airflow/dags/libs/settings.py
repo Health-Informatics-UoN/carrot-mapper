a972416ec0bb12365abb40961f3a5815630b32cf
Brian Kiiru Kim <kiirubrian21@gmail.com>
2026-01-21 14:02:37 +0000

    Optimised bulk inserts in scan report processing to increase upload speed prevent timeouts (#1242)

--- app/airflow/dags/libs/settings.py
+++ app/airflow/dags/libs/settings.py
@@ -17,4 +17,7 @@ SEARCH_ENABLED = os.getenv("SEARCH_ENABLED", "false").lower()
 # Timedelta for dagrun_timeout in minutes
 AIRFLOW_DAGRUN_TIMEOUT = os.getenv("AIRFLOW_DAGRUN_TIMEOUT", 60)
 
+# Page size for bulk database inserts (execute_values)
+EXECUTE_VALUES_PAGE_SIZE = int(os.getenv("EXECUTE_VALUES_PAGE_SIZE", 10000))
+
 AIRFLOW_VAR_JSON_VERSION = os.getenv("AIRFLOW_VAR_JSON_VERSION", "v1")
