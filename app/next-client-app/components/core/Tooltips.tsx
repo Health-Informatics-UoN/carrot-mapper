ca7358218facd90a344d6886504635894d0a9e27
Brian <kiirubrian21@gmail.com>
2026-03-18 13:48:41 +0000

    Fix suggestions from review

--- app/next-client-app/components/core/Tooltips.tsx
+++ app/next-client-app/components/core/Tooltips.tsx
@@ -6,14 +6,13 @@ import {
 } from "@/components/ui/tooltip";
 
 import { InfoIcon } from "lucide-react";
-import { ReactElement } from "react";
 
 export function Tooltips({
   content,
   link,
   side = "top",
 }: {
-  content: string | ReactElement;
+  content: string;
   link?: string;
   side?: "top" | "bottom" | "left" | "right";
 }) {
@@ -27,28 +26,24 @@ export function Tooltips({
           className="max-w-96 text-center whitespace-pre-wrap"
           side={side}
         >
-          {typeof content === "string" ? (
-            <p>
-              {content}
-              {link && (
-                <>
-                  {" "}
-                  Find out more{" "}
-                  <a
-                    href={link}
-                    className="underline"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    here
-                  </a>
-                  .
-                </>
-              )}
-            </p>
-          ) : (
-            content
-          )}
+          <p>
+            {content}
+            {link && (
+              <>
+                {" "}
+                Find out more{" "}
+                <a
+                  href={link}
+                  className="underline"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  here
+                </a>
+                .
+              </>
+            )}
+          </p>
         </TooltipContent>
       </Tooltip>
     </TooltipProvider>
